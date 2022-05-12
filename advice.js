a2d714ebca60736a90a0e8b00dea6996f5e53980
corsfrancken <corsfr@gmail.com>
2022-05-12 17:02:22 +0200

    advice change

--- advice.js
+++ advice.js
@@ -1,27 +1,21 @@
-const resDiv = document.querySelector('#results');
-const resBtn = document.querySelector('#getData');
+var advies = [];
+advies[0] = "Laat je niet kisten.";
+advies[1] = "Houd altijd een boom tussen jou en een beer.";
+advies[2] = "Gas is erg duur geworden.";
+advies[3] = "Elk nadeel heb z'n voordeel.";
+advies[4] = "Als je niet ken winnen, moet je zorgen dat je niet verliest.";
+advies[5] = "Vaak moet er iets gebeuren voordat er iets gebeurt.";
+advies[6] = "Waardeer wat je hebt.";
+advies[7] = "Geef niet op.";
+advies[8] = "Een dag niet gelachen is een dag niet geleefd.";
+advies[9] = "Haal geen oude koeien uit de sloot.";
+advies[10] = "Pak wat je pakken kan.";
 
-
-
-
-resBtn.addEventListener('click',() => {
-    getAdvice();
-});
-
-window.onload = () => {
-    getAdvice();
-};
-
-function getAdvice() {
-
-
-    fetch(' https://api.adviceslip.com/advice').then(response => {
-        return response.json();
-    }).then(adviceData => {
-        const Adviceobj = adviceData.slip;
-        resDiv.innerHTML = `<p>${Adviceobj.advice}</p>`;
-    }).catch(error => {
-        console.log(error);
-    });
-
+function adviesGeven() {
+    var geefAdvies = Math.floor(Math.random()*(11));
+    document.getElementById("results").innerHTML = advies[geefAdvies];
 }
+
+
+document.getElementById("adviesKnop").addEventListener("click", adviesGeven);
+
